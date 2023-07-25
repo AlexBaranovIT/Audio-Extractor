4870c5609e70c19986ea02f400b677616aa11c39
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-07-25 07:21:50 +0300

    Update audioconv.py

--- audioconv.py
+++ audioconv.py
@@ -1,11 +1,12 @@
 import moviepy.editor
 from pathlib import Path
 
-
+#Path to your video file
 video_file = Path('nameofyourvideofile.mp4')
 
 
 video = moviepy.editor.VideoFileClip(f'{video_file}')
 audio = video.audio
 
+#Writes new audio
 audio.write_audiofile(f'{video_file.stem}.mp3')
