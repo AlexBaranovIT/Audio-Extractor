81353287403a37a0fa2e3a6bd5413ed41e47fbc2
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-07-28 13:34:34 +0300

    Update audioconv.py

--- audioconv.py
+++ audioconv.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 #Path to your video file
 video_file = Path('nameofyourvideofile.mp4')
-
 #Variable for video file
 video = moviepy.editor.VideoFileClip(f'{video_file}')
 #Variable for new audio
