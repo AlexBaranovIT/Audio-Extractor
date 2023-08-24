bdbecf170df97e983e91d925ec56caddc9a6007d
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-08-24 11:48:00 +0300

    Update audioconverter.py

--- audioconverter.py
+++ audioconverter.py
@@ -1,7 +1,7 @@
 import moviepy.editor
 from pathlib import Path
 
-#Path to your video file
+#Path to your video file 
 video_file = Path('nameofyourvideofile.mp4')
 #Variable for video file
 video = moviepy.editor.VideoFileClip(f'{video_file}')
